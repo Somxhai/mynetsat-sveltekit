8eca44acb9982407dc2a4ccf7f486f2465d0da3c
= <=>
2023-03-02 08:43:48 +0700

    added settings and theme writable

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,12 +1,25 @@
 import { writable } from 'svelte/store';
-import type { Dialogues, NetsatType, Weight } from './types';
+import type { Dialogues, NetsatType, Scores, Settings, Theme } from './types';
+import { browser } from '$app/environment';
 
 export const dialogue = writable<Dialogues>('NONE');
 
 export const selectFaculty = writable<Set<NetsatType>>(new Set());
 
-export const scores = writable<Weight>(<Weight>{});
+export const scores = writable<Scores>(<Scores>{});
 
 export const calculate = writable(false);
 
-export const netsat = writable<NetsatType[]>();
+export const netsatData = writable<NetsatType[]>();
+
+export const settings = writable<Settings>(<Settings>{});
+export const theme = writable<Theme>('light');
+// listener
+settings.subscribe(async (value) => {
+	if (browser) {
+		await fetch('/settings', {
+			method: 'POST',
+			body: JSON.stringify(value)
+		});
+	}
+});
