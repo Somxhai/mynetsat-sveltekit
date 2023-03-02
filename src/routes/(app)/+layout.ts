772ca33f9ac9648ea2bcde3bc6a99a5b82b8a06a
= <=>
2023-03-02 08:52:17 +0700

    removed

--- src/routes/(app)/+layout.ts
+++
@@ -1,9 +0,0 @@
-import type { NetsatAPI } from '$lib/types';
-import type { LayoutLoad } from './$types';
-
-export const load: LayoutLoad = async ({ fetch }) => {
-	const netsatRequest = fetch('https://netsat-api.vercel.app/');
-	const netsatApi = await (await netsatRequest).text();
-
-	return JSON.parse(netsatApi) as NetsatAPI;
-};
