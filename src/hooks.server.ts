7ec379887aba566d1b7b8698198c913657b17b50
= <=>
2023-03-02 08:43:24 +0700

    added hooks for theme switching

---
+++ src/hooks.server.ts
@@ -0,0 +1,14 @@
+import type { Theme } from '$lib/types';
+import type { Handle } from '@sveltejs/kit';
+
+export const handle: Handle = async ({ event, resolve }) => {
+	const theme = (event.cookies.get('theme') ?? 'light') as Theme;
+	event.locals.theme = theme
+	if (theme) {
+		return await resolve(event, {
+			transformPageChunk: ({ html }) => html.replace('%theme%', theme)
+		});
+	}
+
+	return await resolve(event);
+};
