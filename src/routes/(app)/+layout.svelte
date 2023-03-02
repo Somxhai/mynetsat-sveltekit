7695dba611a7649662cf4c83c6a3940d7a4a3985
= <=>
2023-03-02 08:51:09 +0700

    set writable on server

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,22 +1,22 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
-	import { dialogue, netsat } from '$lib/stores';
-	import Navbar from '$lib/ui/Navbar.svelte';
-	import { onMount } from 'svelte';
+
+	import { dialogue, netsatData, settings, theme } from '$lib/stores';
+	import Navbar from '$lib/ui/navbar/Navbar.svelte';
 
 	import '../../app.css';
 	import type { LayoutData } from './$types';
 
 	export let data: LayoutData;
 
-	onMount(() => {
-		netsat.set(data.netsat);
-	});
+	settings.set(data.settings);
+	theme.set(data.theme);
+	netsatData.set(data.netsatApi.netsat);
 
 	$: if (browser) document.body.style.overflow = $dialogue != 'NONE' ? 'hidden' : 'auto';
 </script>
 
 <Navbar />
-<main class="relative py-6 top-16 lg:gap-10 lg:py-10">
+<main class="relative py-6 top-16 lg:gap-10 lg:py-10 dark:bg-black dark:text-slate-100 pb-[8rem]">
 	<slot />
 </main>
