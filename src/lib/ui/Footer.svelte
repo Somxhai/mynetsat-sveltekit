1cbd46f73f874229780092c78fe18d92d5b6a067
= <=>
2023-03-05 14:56:26 +0700

    changes to open in new tab and refactor dark class

--- src/lib/ui/Footer.svelte
+++ src/lib/ui/Footer.svelte
@@ -1,3 +1,8 @@
-<footer class="dark:text-slate-100 text-black text-center">
-	Maintaining By <a href="https://www.instagram.com/peaktoleast/" class="underline text-blue-500">Somxhai</a>
+<footer class="text-black text-center dark:text-slate-100">
+	Maintaining By <a
+		target="_blank"
+		rel="noreferrer"
+		href="https://www.instagram.com/peaktoleast/"
+		class="underline text-blue-500">Somxhai</a
+	>
 </footer>
