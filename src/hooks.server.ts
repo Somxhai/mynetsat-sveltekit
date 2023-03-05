456fd889fbbdba63776bcafa024c558de310647d
= <=>
2023-03-05 14:54:43 +0700

    added colon to theme

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,7 +3,7 @@ import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const theme = (event.cookies.get('theme') ?? 'light') as Theme;
-	event.locals.theme = theme
+	event.locals.theme = theme;
 	if (theme) {
 		return await resolve(event, {
 			transformPageChunk: ({ html }) => html.replace('%theme%', theme)
