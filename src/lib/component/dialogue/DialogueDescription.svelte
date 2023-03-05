c58fbfb27c9f20b40b90e615f1f222e232327bf1
= <=>
2023-03-05 15:06:29 +0700

    added margin bottom 2rem

--- src/lib/component/dialogue/DialogueDescription.svelte
+++ src/lib/component/dialogue/DialogueDescription.svelte
@@ -1,3 +1,3 @@
-<p class="text-xs md:text-sm text-center md:text-left text-slate-500 dark:text-slate-100 mb-2">
+<p class="text-xs md:text-sm text-center md:text-left text-slate-500 mb-2 dark:text-slate-400">
 	<slot />
 </p>
